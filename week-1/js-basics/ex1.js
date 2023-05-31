2abc428306ad1f6ccfb8d127ab143f34c4c827be
ilmar521 <ilmar521@mail.ru>
2023-05-31 18:19:17 +0300

    commit

--- week-1/js-basics/ex1.js
+++ week-1/js-basics/ex1.js
@@ -1,15 +1,19 @@
-let boughtTesla = true
-const yearOfTeslaPurchase = 2014
-let isUSCitizen = true
+let boughtTesla = prompt('Do you have the Tesla? (y/n)')
 let currentYear = 2018
 
-if (boughtTesla) {
-    if (isUSCitizen) {
-        yearsOfOwning = currentYear - yearOfTeslaPurchase;
-        if (yearsOfOwning > 4) {
-            console.log('Would you like to upgrade your car?');
+if (boughtTesla == 'y') {
+    let isUSCitizen = prompt('Are you US Citizen? (y/n)')
+    if (isUSCitizen == 'y') {
+        const yearOfTeslaPurchase = Number(prompt('When did you buy your Tesla?'))
+        if (isNaN(yearOfTeslaPurchase)) {
+            alert("You must enter only numbers!");
         } else {
-            console.log('Are you satisfied with your car?');   
+            yearsOfOwning = currentYear - yearOfTeslaPurchase;
+            if (yearsOfOwning > 4) {
+                console.log('Would you like to upgrade your car?');
+            } else {
+                console.log('Are you satisfied with your car?');   
+            }
         }
     } else {
         console.log('Would you like to move to the US?');      
