192f2e1d7b150f169d6c1e84992756e0265631ef
ilmar521 <118733735+ilmar521@users.noreply.github.com>
2023-06-14 17:46:54 +0300

    commit visual overload extension

--- week-3/dom-intro/Visual-overload/script.js
+++ week-3/dom-intro/Visual-overload/script.js
@@ -8,12 +8,32 @@ const getRandomColor = function () {
     }
     return color;
 }
+const checkAllBoxesColors = function () {
+    const boxes = document.getElementsByClassName('box')
+    let allSame = true
+    for (let i = 1; i < 10; i++) {
+        if (boxes[i - 1].style.backgroundColor != boxes[i].style.backgroundColor) {
+            allSame = false
+            break    
+        }
+    }
+    if (allSame) {
+        let title = document.querySelector("h1")
+        if (title) {
+            title.remove()
+        }
+        title = document.createElement("h1")  
+        title.innerText = 'Nice job!'
+        container.insertBefore(title, container.firstChild)
+    } 
+}
 
 for (let i = 0; i < 10; i++) {
     let box = document.createElement("div")  
     box.classList.add('box')
     box.addEventListener('mouseenter', function () {
         this.style.backgroundColor = getRandomColor()
+        checkAllBoxesColors()
     })
     container.appendChild(box)
 }
