27abc0a0ab4dbe12d1d3f987e400d7d08222437c
ilmar521 <118733735+ilmar521@users.noreply.github.com>
2023-07-03 18:21:21 +0300

    intro to json

---
+++ week-6/intro-to-json/ex3.js
@@ -0,0 +1,13 @@
+
+let animal = {
+    "name": "",
+    "weigh": {
+        "value": 0,
+        "type": ""
+    },
+    "area_of_living": "",
+    "endangered": false,
+    "preferable_food": [],
+    "animal_that_eat_it": [],
+    "score_of_delicious": 1,
+}
---
+++ week-6/intro-to-json/ex4.js
@@ -0,0 +1,13 @@
+
+let squirrel = {
+    "name": "Squirrel",
+    "weigh": {
+        "value": 300,
+        "type": "gramm"
+    },
+    "area_of_living": "forest",
+    "endangered": false,
+    "preferable_food": ["nuts", "insects", "grains"],
+    "animal_that_eat_it": ["fox", "owl"],
+    "score_of_delicious": 2,
+}
