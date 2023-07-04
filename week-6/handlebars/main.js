9603e0da13e5e43353710f692c94bcadb6ed9f7b
ilmar521 <118733735+ilmar521@users.noreply.github.com>
2023-07-04 20:54:18 +0300

    handlebar

---
+++ week-6/handlebars/main.js
@@ -0,0 +1,24 @@
+
+$("button").on("click", function () {
+    let address = $("#addr-input").val()
+    let minPrice = $("#min-p-input").val()
+    let maxPrice = $("#max-p-input").val()
+    let minRooms = $("#min-r-input").val()
+    let maxRooms = $("#max-r-input").val()
+    let immediate = $("#immed-y")
+
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    renderApts(relevantApts)
+})
+
+const renderApts = function (apartments) {
+    $("#results").empty()
+
+    const source = $('#results-template').html();
+    const template = Handlebars.compile(source);
+    const newHTML = template({ apartment: apartments });
+    
+    $('#results').append(newHTML);
+}
+
+renderApts(apartments)
