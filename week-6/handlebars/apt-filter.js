c1c6f5a888d4118c429fb99b4e4b109cd3b3443d
ilmar521 <118733735+ilmar521@users.noreply.github.com>
2023-07-05 08:17:47 +0300

    handlebars, new features

--- week-6/handlebars/apt-filter.js
+++ week-6/handlebars/apt-filter.js
@@ -2,15 +2,19 @@
 
 const must = (radio) => radio.filter(rb => rb.checked).length
 
-const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate) {
+const findRelevantApts = function (address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking) {
+
+    immediate = !immediate[0].checked
+    parking = !parking[0].checked
     let relevantApts = apartments.filter(a =>
         a.address.toLowerCase().includes(address.toLowerCase() || "") &&
         a.price >= (minPrice || 0) &&
         a.price <= (maxPrice || a.price) &&
         a.rooms >= (minRooms || 0) &&
-        a.rooms <= (maxRooms || a.rooms)
+        a.rooms <= (maxRooms || a.rooms) &&
+        (a.immediate || immediate) &&
+        (a.parking || parking)
     )
 
-    immediate = !immediate[0].checked
-    return immediate ? relevantApts : relevantApts.filter(a => (a.immediate)) 
+    return relevantApts
 }
--- week-6/handlebars/main.js
+++ week-6/handlebars/main.js
@@ -6,8 +6,9 @@ $("button").on("click", function () {
     let minRooms = $("#min-r-input").val()
     let maxRooms = $("#max-r-input").val()
     let immediate = $("#immed-y")
+    let parking = $("#park-y")
 
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
+    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate, parking)
     renderApts(relevantApts)
 })
 
