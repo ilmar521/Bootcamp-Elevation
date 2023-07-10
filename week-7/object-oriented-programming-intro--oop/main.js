3f8a3ebb01a7e76fa5d2e5192046402e0cd55d7c
ilmar521 <118733735+ilmar521@users.noreply.github.com>
2023-07-10 15:49:50 +0300

    OOP intro

---
+++ week-7/object-oriented-programming-intro--oop/main.js
@@ -0,0 +1,68 @@
+//write your code here to make the tests pass
+
+class Document {
+    constructor(EmployeeName) {
+        this.EmployeeName = EmployeeName
+    }
+}
+
+class Employee {
+    constructor(name) {
+        this.name = name
+    }
+    work(office) {
+        for (let i = 0; i < 10; i++) {
+            office.documents.push(new Document(this.name))              
+        }
+    }
+}
+
+class Manager {
+    constructor(name) {
+        this.name = name
+        this.employees = []
+    }
+    hireEmployee(name) {
+        this.employees.push(new Employee(name))
+    }
+    askEmployeesToWork(office) {
+        for (const employee of this.employees) {
+            employee.work(office)   
+        }
+    }
+}
+
+
+class Cleaner {
+    constructor(name) {
+        this.name = name
+    }
+    clean() {
+        console.log('Clean')
+    }
+}
+
+class Office {
+
+    constructor() {
+        this.documents = []
+        this.managers = []
+        this.cleaners = []
+    }
+ 
+    hireCleaner(name) {
+        this.cleaners.push(new Cleaner(name))
+    }
+    hireManager(name) {
+        this.managers.push(new Manager(name))
+    }
+    startWorkDay() {
+        for (const manager of this.managers) {
+            manager.askEmployeesToWork(this)
+        }
+        for (const cleaner of this.cleaners) {
+            cleaner.clean()
+        }
+    }
+}
+
